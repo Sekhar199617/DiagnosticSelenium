438a4721c34fb52ac86215a9ac3c0f381c73e57c
sagarks826 <sagar.pare@ksolves.com>
2025-02-03 10:09:22 +0530

    Made changes in create account test

--- src/test/java/pageObjects/CreateAccountPage.java
+++ src/test/java/pageObjects/CreateAccountPage.java
@@ -12,56 +12,24 @@ public class CreateAccountPage extends  BasePage{
         super(driver);
         commonUtils = new CommonUtils(driver);
     }
-    @FindBy(xpath = "//button[normalize-space()='Create Account']")
-    public WebElement clickCreateAccount;
 
-    @FindBy (xpath = "//input[@placeholder='Account Name']")
-    public WebElement  accountNameField;
-
-    @FindBy (xpath = "//input[@placeholder='Account Admin Name']")
-    public WebElement  primaryContactNameField;
-
-    @FindBy (xpath = "//input[@placeholder='Email']")
-    public WebElement  emailField;
-
-    @FindBy (xpath = "//input[@placeholder='Phone']")
-    public WebElement  phoneField;
-
-    @FindBy (xpath = "//input[@id='company']")
-    public WebElement accountTypeCompanyRadioButton;
-
-    @FindBy (xpath = "//input[@id='individual']")
-    public WebElement accountTypeIndividualRadioButton;
-
-    @FindBy (xpath = "//select[@id='messaging_id']")
-    public WebElement diagnosticMessagingSetDropdown;
-
-    @FindBy (xpath = "//select[@id='default_intake_form']")
-    public WebElement defaultIntakeFormDropdown;
-
-    @FindBy (xpath = "//select[@id='billing_country']")
-    public WebElement billingCountryDropdown;
-
-    @FindBy (xpath = "//input[@name='billing_line_one']")
-    public WebElement billingAddress_1Field;
-
-    @FindBy (xpath = "//input[@name='billing_line_two']")
-    public WebElement billingAddress_2Field;
-
-    @FindBy (xpath = "//input[@name='city']")
-    public WebElement billingCityField;
-
-    @FindBy (xpath = "//input[@name='state']")
-    public WebElement billingStateField;
-
-    @FindBy (xpath =  "//input[@name='postal_code']")
-    public WebElement billingPostcodeField;
-
-    @FindBy (xpath = "//input[@name='demoUser']")
-    public WebElement demoAccountCheckbox;
-
-    @FindBy (xpath = "//*[@id='addAccountForm']")
-    public WebElement saveNewAccount_button;
+    public String clickCreateAccount= "//button[normalize-space()='Create Account']";
+    public String  accountNameField  = "//input[@placeholder='Account Name']";
+    public String  primaryContactNameField  = "//input[@placeholder='Account Admin Name']";
+    public String  emailField = "//input[@placeholder='Email']";
+    public String  phoneField = "//input[@placeholder='Phone']";
+    public String accountTypeCompanyRadioButton = "//input[@id='company']";
+    public String accountTypeIndividualRadioButton = "//input[@id='individual']";
+    public String diagnosticMessagingSetDropdown = "//select[@id='messaging_id']";
+    public String defaultIntakeFormDropdown = "//select[@id='default_intake_form']";
+    public String billingCountryDropdown = "//select[@id='billing_country']";
+    public String billingAddress_1Field = "//input[@name='billing_line_one']";
+    public String billingAddress_2Field = "//input[@name='billing_line_two']";
+    public String billingCityField = "//input[@name='city']";
+    public String billingStateField = "//input[@name='state']";
+    public String billingPostcodeField =  "//input[@name='postal_code']";
+    public String demoAccountCheckbox = "//input[@name='demoUser']";
+    public String saveNewAccount_button = "//*[@id='addAccountForm']";
 
 
 }
