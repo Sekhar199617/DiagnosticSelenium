00fb163f2590c095c641d1e2c430c3a8da3fc1e5
sagarks826 <sagar.pare@ksolves.com>
2025-02-12 17:11:59 +0530

    Resolved PR conflicts

--- src/test/java/testCases/PurchaserPrecreatedCollectionInFormDataTest.java
+++ src/test/java/testCases/PurchaserPrecreatedCollectionInFormDataTest.java
@@ -1,17 +1,17 @@
     package testCases;
 
-    import org.testng.Assert;
-    import org.testng.annotations.Test;
-    import org.testng.annotations.AfterMethod;
-    import org.testng.annotations.BeforeMethod;
-    import pageObjects.AccountDetailsAddFormsPage;
-    import pageObjects.AccountDetailsPage;
-    import pageObjects.DashboardPage;
-    import pageObjects.PurchaseLevelAccountPage;
-    import testBase.BaseClass;
-    import utilities.CommonUtils;
-    import java.util.ArrayList;
-    import java.util.List;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pageObjects.AccountDetailsAddFormsPage;
+import pageObjects.AccountDetailsPage;
+import pageObjects.DashboardPage;
+import pageObjects.PurchaseLevelAccountPage;
+import testBase.BaseClass;
+import utilities.CommonUtils;
+import java.util.ArrayList;
+import java.util.List;
 
     public class PurchaserPrecreatedCollectionInFormDataTest extends BaseClass {
 
