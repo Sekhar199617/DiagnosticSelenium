e99d2ac8ce6da1055f70cbb5d9cf5b5327452903
sagarks826 <sagar.pare@ksolves.com>
2025-02-26 11:57:58 +0530

    Added separate method for edit event

--- src/test/java/testCases/AccountDetailsAddEventTest.java
+++ src/test/java/testCases/AccountDetailsAddEventTest.java
@@ -1,36 +1,51 @@
 package testCases;
 
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import pageObjects.AccountDetailsPage;
 import pageObjects.AccountDetailsAddEventTagsPage;
 import pageObjects.DashboardPage;
+import pageObjects.PurchaseLevelAccountPage;
 import testBase.BaseClass;
 import utilities.CommonUtils;
 
 public class AccountDetailsAddEventTest extends BaseClass {
 
-    @Test(groups = { "Smoke" })
+    public DashboardPage dp;
+    public CommonUtils commonUtils;
+    public AccountDetailsPage ad;
+    public AccountDetailsAddEventTagsPage addEventTagsPage;
+    public String formName;
+    public PurchaseLevelAccountPage pl;
+
+    @BeforeMethod (groups = {"Smoke"})
+    public void event_common_step(){
+        logger.info("****** Starting Add Event Test Case ******");
+
+        login(p.getProperty("adminEmail"), p.getProperty("adminPassword"), true);
+
+        dp = new DashboardPage(driver);
+        commonUtils = new CommonUtils(driver);
+        ad = new AccountDetailsPage(driver);
+        addEventTagsPage = new AccountDetailsAddEventTagsPage(driver);
+        pl = new PurchaseLevelAccountPage(driver);
+
+        dp.searchForItem(p.getProperty("accountName"));
+        dp.clickView();
+        commonUtils.selectTab(commonUtils.findElementsByXpath(ad.tabList), "Events/Tags");
+
+    }
+
+    @Test(groups = { "Smoke" }, priority = 1)
     public void verify_add_event() {
         try {
-            logger.info("****** Starting Add Event Test Case ******");
-
-            login(p.getProperty("adminEmail"), p.getProperty("adminPassword"), true);
-
-            DashboardPage dp = new DashboardPage(driver);
-            CommonUtils commonUtils = new CommonUtils(driver);
-
-            dp.searchForItem(p.getProperty("accountName"));
-            dp.clickView();
-
-            AccountDetailsPage ad = new AccountDetailsPage(driver);
-            commonUtils.selectTab(commonUtils.findElementsByXpath(ad.tabList), "Events/Tags");
-
-            AccountDetailsAddEventTagsPage addEventTagsPage = new AccountDetailsAddEventTagsPage(driver);
+            formName = randomString();
             // Fill Event Details
             commonUtils.clickOnElement(commonUtils.findElementByXpath(addEventTagsPage.addEventButton), null);
-            commonUtils.enterValueInTextField(commonUtils.findElementByXpath(addEventTagsPage.eventNameTagField), randomString() );
+            commonUtils.enterValueInTextField(commonUtils.findElementByXpath(addEventTagsPage.eventNameTagField), formName );
             commonUtils.enterValueInTextField(commonUtils.findElementByXpath(addEventTagsPage.eventStartDateCalender), p.getProperty("eventStartDate"));
             commonUtils.enterValueInTextField(commonUtils.findElementByXpath(addEventTagsPage.eventEndDateCalender), p.getProperty("eventEndDate") );
             commonUtils.enterValueInTextField(commonUtils.findElementByXpath(addEventTagsPage.eventStartTime), p.getProperty("eventStartTiming") );
@@ -46,6 +61,19 @@ public class AccountDetailsAddEventTest extends BaseClass {
             logger.error("Error occurred: " + e.getMessage());
             Assert.fail("Test failed due to exception.");
         }
+    }
+
+    @Test (groups = {"Smoke"}, priority = 2)
+    public void verify_Edit_Event() throws InterruptedException {
+
+        pl.performTableAction("accountsTable", formName, "Edit",1);
+        Thread.sleep(2000);
+
+    }
+
+    @AfterMethod
+    public void deleteCookies(){
+        driver.manage().deleteAllCookies();
     }
 
 }
